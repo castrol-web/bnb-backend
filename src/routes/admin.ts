9971524dbed379cda010fa69b7a47ef1d1b8d8e1
castrol-web <castrol.banda@students.jkuat.ac.ke>
2025-07-01 15:10:15 +0300

    headers

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -9,6 +9,7 @@ import Room from "../models/Room"
 import bcrypt from "bcryptjs";
 import dotenv from "dotenv";
 import Review from "../models/Review";
+import authMiddleware from "../middleware/auth.middleware";
 dotenv.config();
 const router = express.Router();
 
@@ -66,7 +67,7 @@ const upload = multer({ storage });
 const randomImageName = (bytes = 32) => crypto.randomBytes(bytes).toString("hex");
 
 // CREATE a room
-router.post('/create-room', upload.fields([{ name: 'pictures', maxCount: 10 }, { name: 'frontViewPicture', maxCount: 1 }]), async (req: Request, res: any) => {
+router.post('/create-room',authMiddleware, upload.fields([{ name: 'pictures', maxCount: 10 }, { name: 'frontViewPicture', maxCount: 1 }]), async (req: Request, res: any) => {
     try {
         const {
             title,
@@ -142,7 +143,7 @@ router.post('/create-room', upload.fields([{ name: 'pictures', maxCount: 10 }, {
 
 
 // UPDATE a room
-router.put('/room/:id', upload.fields([{ name: 'pictures', maxCount: 10 }, { name: 'frontViewPicture', maxCount: 1 },]),
+router.put('/room/:id',authMiddleware, upload.fields([{ name: 'pictures', maxCount: 10 }, { name: 'frontViewPicture', maxCount: 1 },]),
     async (req: Request, res: any) => {
         try {
             const room = await Room.findById(req.params.id);
@@ -258,7 +259,7 @@ router.put('/room/:id', upload.fields([{ name: 'pictures', maxCount: 10 }, { nam
 
 
 // DELETE a room
-router.delete('/room/:id', async (req: Request, res: any) => {
+router.delete('/room/:id',authMiddleware, async (req: Request, res: any) => {
     try {
         const room = await Room.findById(req.params.id);
         if (!room) {
